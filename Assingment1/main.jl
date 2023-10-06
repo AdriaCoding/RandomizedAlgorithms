7ab67cdcc3a104c9c7b92032e97d0af064ee4491
jake00100 <adrialisa99@gmail.com>
2023-10-06 22:38:15 +0200

    Fixed Scoping error

--- Assingment1/main.jl
+++ Assingment1/main.jl
@@ -1,12 +1,16 @@
-using Pkg
+import Pkg
+Pkg.add("Plots")
 
-N::UInt128 = 100
-darts-thrown::UInt128 = 0
-🎯::UInt128 = 0
+N = 100000000;
+🎯 = 0; π = 0
 
-while darts-thrown <= N
+for darts in 1:N
     # generate random doubles x, y ∈ [0, 1)
     x, y = rand(Float64, 2) 
-    if x^2 + y^2 < 1
-        
-
+    if x^2 + y^2 <= 1
+        global 🎯 += 1
+    end
+    darts += 1
+    global π = 4*🎯/darts
+end
+println("Estimated value of π: ", π)
