57788f3ec637c7ef0716f8783bca13f3050916d8
jake00100 <adrialisa99@gmail.com>
2023-10-06 20:35:39 +0200

    Generating random numbers

--- Assingment1/main.jl
+++ Assingment1/main.jl
@@ -1,1 +1,12 @@
-print("Hello World!")
+using Pkg
+
+N::UInt128 = 100
+darts-thrown::UInt128 = 0
+🎯::UInt128 = 0
+
+while darts-thrown <= N
+    # generate random doubles x, y ∈ [0, 1)
+    x, y = rand(Float64, 2) 
+    if x^2 + y^2 < 1
+        
+
