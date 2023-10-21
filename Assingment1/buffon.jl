44bf6d3707a137c8a543435ee9b45ff750934c22
jake00100 <adrialisa99@gmail.com>
2023-10-21 11:34:50 +0200

    Started working with module RandomPi

--- Assingment1/buffon.jl
+++ Assingment1/buffon.jl
@@ -27,17 +27,19 @@ function fast_buffon(N)
         return (2*l*N)/(t*C)
     end
 end
+
+# auxiliary function for master_buffon
 function count_buffon(N)
-    xs = (t/2) * rand(N)
-    θs = (π/2) * rand(N)
+    xs = (t/2) * rand(Float64, N)
+    θs = (π/2) * rand(Float64, N)
     return count(xs .≤ (l/2) * sin.(θs))
 end
+
 # a mix of fast and naive methods. 
 function master_buffon(N, max_n = 10^6)
     # if N ≤ max_n , master_buffon ≡ fast_buffon
     global C_global = count_buffon(N % max_n)
     for i in 1:(N ÷ max_n)
-        println(count_buffon(max_n))
         global C_global += count_buffon(max_n)
     end
     if C_global == 0
@@ -46,38 +48,3 @@ function master_buffon(N, max_n = 10^6)
         return (2*l*N)/(t*C_global)
     end
 end
-
-function ϵ(p)
-    return 
-end
-
-using Plots
-using LaTeXStrings
-function plot_err_buffon(e)
-    N = 10^e
-    tic = 10.0 .^ collect(1:e)  
-    
-    p = master_buffon.(1:N)
-    p_abs = plot(1:N, p, 
-        xscale = :log10,
-        legend = false,
-        xlims = [0, N],
-        ylims = [0, 2*pi],
-        xticks =  tic
-    )
-
-    plot!(p_abs, [0, N], [pi, pi],
-        linecolor = :red,
-        linestyle = :dash
-    )
-
-    ϵ = abs,(p .-pi)/pi 
-    p_rel = plot(1:N, ϵ,
-        xscale = :log10,
-        yscale = :log10,
-        legend = false,
-        xlims = (0, N)
-    )
-    display(p_abs)
-end
-plot_err_buffon(4)
--- Assingment1/main.jl
+++ Assingment1/main.jl
@@ -4,6 +4,38 @@ include("../Assingment1/buffon.jl")
 #using Pkg
 #Pkg.add("DataStructures") # otherwise count() can bug
 using BenchmarkTools #used to test out performance
+using Plots
+pyplot()
+using LaTeXStrings
 
-N = 10^3
-plot_err_buffon()
+function plot_err(e, pi_function)
+    N = 10^e
+    rng = collect(0:10^(e-3):N)    
+    p = pi_function.(rng)
+    p_abs = plot(rng, p,
+        legend = false,
+        xlims = [0, 10^e],
+        ylabel = L"Approximation",
+        ylims = [3.1, 3.2],
+        xlabel = L"$N$"
+    )
+    xaxis!(p_abs, 
+        minorgrid = true,
+    )
+
+    plot!(p_abs, [0, N], [pi, pi],
+        linecolor = :red,
+        linestyle = :dash
+    )
+
+    rerr = abs.(p .-pi)/pi 
+    p_rel = plot(rng, rerr,
+        yscale = :log10,
+        ylabel = "Relative Error",
+        legend = false,
+        xlabel = L"$N$",
+    )
+    display(p_abs)
+    display(p_rel)
+end
+@time plot_err(6, master_buffon)
