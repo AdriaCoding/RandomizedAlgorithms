2bf2fe49c5481dec44394d3a8270e7957bcea4f5
AdriaCoding <adrialisa99@gmail.com>
2023-12-08 20:40:01 +0100

    KMV push!

--- Assignment3/KMV.jl
+++ Assignment3/KMV.jl
@@ -1,9 +1,9 @@
 import DataStructures: SortedSet
 struct MyKMV{k}
-    values::SortedSet{UInt8}
+    values::SortedSet{UInt}
     function MyKMV{k}() where k
         isa(k, Integer) || throw(ArgumentError("k must be an integer"))
-        return new(SortedSet{UInt8}(Base.Order.Reverse))
+        return new(SortedSet{UInt}(Base.Order.Reverse))
     end
     function MyKMV{k}(initial_values) where k
         kmv = MyKMV{k}()
@@ -16,5 +16,24 @@ struct MyKMV{k}
     end
 end
 MyKMV() = MyKMV{3}()
-MyKMV{3}([0x00, 0x02, 0x01])
+a = MyKMV{3}([0x00, 0x02, 0x01])
+push!(a, 4)
+Base.max(kmv::MyKMV) = first(kmv.values)
 
+function Base.push!(kmv::MyKMV{k}, x) where {k}
+    h = hash(x)
+    if length(kmv.values) < k
+        insert!(kmv.values, h)
+    elseif h < max(kmv)
+        insert!(kmv.values, h)
+        pop!(kmv.values)
+    end
+    return kmv.values
+end
+
+function Base.push!(kmv::MyKMV, entries...)
+    for entry in entries
+        push!(kmv, entry)
+    end
+    return kmv.values
+end
